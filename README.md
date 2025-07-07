a2ec6e5da9d0bf90d9de439bedbd84ab376ee743
Jose Arozena <j.arocenap@gmail.com>
2025-07-07 09:04:11 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Symfony Framework 2025
+
+
+This is a project to practise Symfony + Twig and review the updates in new versions of the framework. Also, the environment will be set up using docker in MacOs. It has been made folowing the course [Symfony Framework Hands-On 2025]([https://www.udemy.com/share/105uii3@L1bD0yjXW69Z8Z6Iapf6SRzlmvvTaUMpH7jhr-VIdoJyYAKgxRHARR7OAc8LQOdPww==/](https://www.udemy.com/course/symfony-framework-hands-on/)) 
+by [Piotr Jura]([https://github.com/gavilanch](https://github.com/piotr-jura-udemy)).
+
+Technologies:
+
+  ![PHP](https://img.shields.io/badge/PHP-777BB4?style=flat&logo=php&logoColor=white)  
+  
+  ![Symfony](https://img.shields.io/badge/Symfony-black?style=for-the-badge&logo=symfony&logoColor=white) 
+  
+  ![MariaDB](https://img.shields.io/badge/MariaDB-003545?logo=mariadb&logoColor=white)
+
+  ![Docker](https://img.shields.io/badge/Docker-2496ED?logo=docker&logoColor=fff)
+
+  ![macOS](https://img.shields.io/badge/macOS-000000?logo=apple&logoColor=F0F0F0)
---
+++ src/Controller/MicroPostController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class MicroPostController extends AbstractController
+{
+    #[Route('/micro-post', name: 'app_micro_post')]
+    public function index(MicroPostRepository $posts, EntityManagerInterface $entityManager): Response
+    {      
+        return $this->render('micro_post/index.html.twig', [
+            'posts' => $posts->findAll(),
+        ]);
+    }
+
+    #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    public function showOne(MicroPost $post): Response
+    {
+        return $this->render('micro_post/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
+    public function add(): Response 
+    {
+        $microPost = new MicroPost();
+
+        $form = $this->createFormBuilder($microPost)
+            ->add('title')
+            ->add('text')
+            ->add('submit', SubmitType::class, ['label' => 'save'])
+            ->getForm();
+
+        return $this->render(
+            'micro_post/add.html.twig',
+            [
+                'form' => $form
+            ]
+        );
+
+    }
+}
